e4b00bbb4b06cb08b5519c2a8e8980aea1d03356
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2019-02-09 09:45:24 +0000

    Ajoutez des fichiers projet.

---
+++ OcDjangoDisquaire/env-django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Cheick\Source\Repos\OcDjangoDisquaire\OcDjangoDisquaire\env-django\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
